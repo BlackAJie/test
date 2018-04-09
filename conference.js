02bfeaab89c3fd1fd78c7ac3f678f12fbf477b31
AJie <jiezi2005@163.com>
2018-04-09 14:23:49 +0800

    work

---
+++ conference.js
@@ -0,0 +1,11 @@
+function add(obj) {
+    var img = document.createElement("img");
+    img.src = "checked";
+    var parent = obj.parentNode;
+    parent.prepend(img);
+    console.log(obj);
+}
+
+function show(str = "nothing") {
+    console.log(str);
+}
